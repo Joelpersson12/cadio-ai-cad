ce876570381c1e6ad114e413e6038e34d041fc06
Joelpersson12 <fgrdgfdggdgd@gmail.com>
2026-05-08 16:25:33 +0200

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Request(BaseModel):
+    prompt: str
 
 @app.get("/")
 def home():
     return {"status": "Cadio API running"}
 
 @app.post("/generate")
-def generate():
-    return {"status": "ok"}
+def generate(data: Request):
+
+    return {
+        "status": "generated",
+        "message": f"You generated: {data.prompt}"
+    }
