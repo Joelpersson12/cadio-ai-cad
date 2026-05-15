2f89d7821280fc306ad25bd59ade05c1d2a35ff8
Joelpersson12 <fgrdgfdggdgd@gmail.com>
2026-05-15 22:02:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -140,5 +140,6 @@ import uvicorn
 app = FastAPI()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8000"))
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, workers=1)
